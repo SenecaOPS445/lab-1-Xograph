61a5f7f510469a8f15ddbe77da1fa500684ba4bc
Kevin Ho <kho55@myseneca.ca>
2025-03-03 10:43:39 -0500

    completed lab

---
+++ lab1d.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+'''Define the objects'''
+x = 10
+y = 2
+z = 5
+
+'''Calculate the result'''
+result = x + y * z
+
+'''Print the output'''
+print(f"{x} + {y} * {z} = {result}")
